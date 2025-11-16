c86e74fd213e0899d9f97d7416eb7dee0aa8fa20
Omar-Saleh1 <os6100050@gmail.com>
2025-11-16 14:22:44 -0800

    Change env

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,7 +8,7 @@ export const NextOption: NextAuthOptions = {
       name: "Credentials",
       credentials: { email: {}, password: {} },
       async authorize(credentials) {
-        const res = await fetch(`${procces.env.NEXT_PUBLIC_API_URL}/user/signin`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/signin`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(credentials),
